78056b33ea54e7105bd756f15b580d324ed9f522
Nicholas W. M. Ritchie <nicholas.ritchie@nist.gov>
2020-09-20 09:58:03 -0400

    Improving type stability

--- src/hyperspectrum.jl
+++ src/hyperspectrum.jl
@@ -25,7 +25,6 @@ function Base.show(io::IO, m::MIME"text/plain", hss::HyperSpectrum)
     )
 end
 
-
 function Base.show(io::IO, m::MIME"text/html", hss::HyperSpectrum)
     sz = join(repr.(size(hss)), " × ")
     print(
@@ -42,7 +41,7 @@ function Base.show(io::IO, hss::HyperSpectrum)
     )
 end
 
-Base.eltype(hss::HyperSpectrum) = Spectrum{eltype(hss.counts)}
+Base.eltype(hss::HyperSpectrum{T,N}) where {T<:Real, N} = Spectrum{T}
 Base.length(hss::HyperSpectrum) = prod(size(hss))
 #Base.ndims(hss::HyperSpectrum) = ndims(hss.index)
 Base.size(hss::HyperSpectrum) = size(hss.counts)[2:end]
@@ -58,15 +57,15 @@ dose(hss::HyperSpectrum) = hss[:ProbeCurrent] * hss[:LiveTime]
 function Base.getindex(hss::HyperSpectrum{T,N}, idx::Int)::Spectrum{T} where {T<:Real,N}
     props = copy(hss.properties)
     props[:Cartesian] = hss.index[idx]
-    return Spectrum(hss.energy, hss.counts[:, hss.index[idx]], props)
+    return Spectrum(hss.energy, counts()[:, hss.index[idx]], props)
 end
 function Base.getindex(hss::HyperSpectrum{T,N}, idx::Vararg{Int,N})::Spectrum{T} where {T<:Real,N}
     props = copy(hss.properties)
     props[:Cartesian] = CartesianIndex(idx...)
-    return Spectrum(hss.energy, hss.counts[:, idx...], props)
+    return Spectrum(hss.energy, counts(hss)[:, idx...], props)
 end
 function region(hss::HyperSpectrum{T,N}, ranges::AbstractRange...)::HyperSpectrum{T,N} where {T<:Real,N}
-    HyperSpectrum(hss.energy, hss.properties, hss.counts[:, ranges...])
+    HyperSpectrum(hss.energy, hss.properties, counts(hss)[:, ranges...])
 end
 Base.getindex(hss::HyperSpectrum, sy::Symbol) = getindex(hss.properties, sy)
 Base.setindex!(hss::HyperSpectrum, val::Any, sy::Symbol) = setindex!(hss.properties, val, sy)
@@ -75,7 +74,8 @@ NeXLCore.energy(ch::Integer, hss::HyperSpectrum) = energy(ch, hss.energy)
 channel(energy::Real, hss::HyperSpectrum) = channel(energy, hss.energy)
 rangeofenergies(ch::Integer, hss::HyperSpectrum) = (energy(ch, hss.energy), energy(ch + 1, hss.energy))
 properties(hss::HyperSpectrum)::Dict{Symbol,Any} = hss.properties
-counts(hss::HyperSpectrum, ci::CartesianIndex, ch::Int) = hss.counts[ch, ci]
+counts(hss::HyperSpectrum, ci::CartesianIndex, ch::Int) = counts(hss)[ch, ci]
+counts(hss::HyperSpectrum{T,N}) where {T<:Real, N} = convert(Array{T,N+1}, hss.counts)
 
 """
     compress(hss::HyperSpectrum)
@@ -83,24 +83,25 @@ counts(hss::HyperSpectrum, ci::CartesianIndex, ch::Int) = hss.counts[ch, ci]
 Returns a HyperSpectrum with smaller or equal storage space to `hss` without losing any infomation (except AbstractFloat
 which compresses to Float32 with loss of precision).  Can change the storage type and/or reduce the depth of hss.
 """
-function compress(hss::HyperSpectrum)
-    (minval, maxval) = extrema(hss.counts)
-    maxi = mapreduce(ci -> findlast(c -> c != 0.0, hss.counts[:, ci]), max, CartesianIndices(hss))
-    data = maxi < depth(hss) ? hss.counts[1:maxi, axes(hss.counts)[2:end]...] : hss.counts
-    if eltype(hss.counts) in (Int16, Int32, Int64)
-        for newtype in filter(ty -> sizeof(eltype(hss.counts)) > sizeof(ty), (Int8, Int16, Int32))
+function compress(hss::HyperSpectrum{T,N}) where {T<:Real, N}
+    data = counts(hss)
+    (minval, maxval) = extrema(data)
+    maxi = mapreduce(ci -> findlast(c -> c != 0.0, data[:, ci]), max, CartesianIndices(hss))
+    data = maxi < depth(hss) ? data[1:maxi, axes(hss.counts)[2:end]...] : data
+    if eltype(T) in (Int16, Int32, Int64)
+        for newtype in filter(ty -> sizeof(T) > sizeof(ty), (Int8, Int16, Int32))
             if minval >= typemin(newtype) && maxval <= typemax(newtype)
                 return HyperSpectrum(hss.energy, hss.properties, newtype.(data))
             end
         end
-    elseif eltype(hss.counts) in (UInt16, UInt32, UInt64)
-        for newtype in filter(ty -> sizeof(eltype(hss.counts)) > sizeof(ty), (UInt8, UInt16, UInt32))
+    elseif T in (UInt16, UInt32, UInt64)
+        for newtype in filter(ty -> sizeof(T) > sizeof(ty), (UInt8, UInt16, UInt32))
             if maxval <= typemax(newtype)
                 return HyperSpectrum(hss.energy, hss.properties, newtype.(data))
             end
         end
-    elseif eltype(hss.counts) isa Float64
-        if sizeof(eltype(hss.counts)) < sizeof(Float32) && minval >= typemin(Float32) && maxval <= typemax(Float32)
+    elseif T isa Float64
+        if sizeof(T) < sizeof(Float32) && minval >= typemin(Float32) && maxval <= typemax(Float32)
             return HyperSpectrum(hss.energy, hss.properties, Float32.(data))
         end
     end
@@ -114,7 +115,7 @@ Sums a contiguous range of data planes into an Array. The dimension of the resul
 one less than the dimension of the HyperSpectrum.
 """
 function plane(hss::HyperSpectrum, chs::AbstractUnitRange{<:Integer}, normalize = false)
-    res = map(ci -> sum(Float64.(hss.counts[chs, ci])), hss.index)
+    res = map(ci -> sum(Float64.(counts(hss)[chs, ci])), hss.index)
     if normalize
         res /= maximum(res)
     end
@@ -124,11 +125,11 @@ end
 """
    plane(hss::HyperSpectrum, ch::Int, normalize=false)
 
-Sums a contiguous range of data planes into an Array. The dimension of the result is
+Extracts a single plane from a HyperSpectrum. The dimension of the result is
 one less than the dimension of the HyperSpectrum.
 """
 function plane(hss::HyperSpectrum, ch::Int, normalize = false)
-    res = hss.counts[ch, hss.index]
+    res = counts(hss)[ch, hss.index]
     if normalize
         res /= maximum(res)
     end
@@ -141,15 +142,14 @@ end
 
 Compute Bright's Max-Pixel derived signal for the entire HyperSpectrum or a rectanglar sub-region.
 """
-function maxpixel(hss::HyperSpectrum, cis::CartesianIndices)
-    res = zeros(eltype(hss.counts), depth(hss))
-    for ci in cis, i in filter(i -> hss.counts[i, ci] > res[i], eachindex(res))
-        res[i] = hss.counts[i, ci]
+maxpixel(hss::HyperSpectrum) = maxpixel(hss, CartesianIndices(hss))
+function maxpixel(hss::HyperSpectrum{T,N}, cis::CartesianIndices) where {T<:Real, N}
+    data, res = counts(hss), zeros(T, depth(hss))
+    for ci in cis, ch in Base.OneTo(size(res,1))
+        if res[ch] = max(res[ch], data[ch, ci])
     end
     return Spectrum(hss.energy, res, hss.properties)
 end
-maxpixel(hss::HyperSpectrum) = maxpixel(hss, CartesianIndices(hss))
-
 """
     indexofmaxpixel(hss::HyperSpectrum, ch::Int) # at channel `ch`
     indexofmaxpixel(hss::HyperSpectrum) # all channels
@@ -160,11 +160,12 @@ Find the coordinates producing the maximum value in data[ch] or data[:] within '
 spatial dimensions.
 """
 function indexofmaxpixel(hss::HyperSpectrum, ch::Int, cis::CartesianIndices)
-    maxidx, max = cis[1], hss.counts[ch, cis[1]]
+    data = counts()
+    maxidx, max = cis[1], data[ch, cis[1]]
     for idx in cis
-        if hss.counts[ch, idx] > max
+        if data[ch, idx] > max
             maxidx = idx
-            max = hss.counts[ch, idx]
+            max = data[ch, idx]
         end
     end
     return maxidx
@@ -174,10 +175,11 @@ indexofmaxpixel(hss::HyperSpectrum, ch::Int) = indexofmaxpixel(hss, ch, Cartesia
 
 indexofmaxpixel(hss::HyperSpectrum) = indexofmaxpixel(hss, CartesianIndices(hss))
 
-function indexofmaxpixel(hss::HyperSpectrum, cis::CartesianIndices)
-    res, cix = zeros(eltype(hss.counts), depth(hss)), CartesianIndex[hss.index[1] for _ = 1:depth(hss)]
-    for ci in cis, i in filter(i -> hss.counts[i, ci] > res[i], eachindex(res))
-        res[i] = hss.counts[i, ci]
+function indexofmaxpixel(hss::HyperSpectrum{T,N}, cis::CartesianIndices) where {T<:Real, N}
+    res, cix = zeros(T, depth(hss)), CartesianIndex[hss.index[1] for _ = 1:depth(hss)]
+    data = counts(hss)
+    for ci in cis, i in filter(i -> data[i, ci] > res[i], eachindex(res))
+        res[i] = data[i, ci]
         cix[i] = ci
     end
     return cix
@@ -193,8 +195,9 @@ Compute a sum spectrum for all or a subset of the pixels in `hss`.
 function Base.sum(hss::HyperSpectrum{T,N}, mask::Union{BitArray,Array{Bool}}) where {T<:Real,N}
     @assert size(mask) == size(hss) "$(size(mask)) ≠ $(size(hss))"
     RT = T isa Int ? Int64 : Float64
+    data = counts(hss)
     vals = mapreduce(
-        ci -> hss.counts[:, ci],
+        ci -> data[:, ci],
         (a, b) -> a .+= b,
         filter(ci -> mask[ci], hss.index),
         init = zeros(RT, depth(hss)),
@@ -208,7 +211,8 @@ end
 
 function Base.sum(hss::HyperSpectrum{T,N}) where {T<:Real,N}
     RT = T isa Int ? Int64 : Float64
-    vals = mapreduce(ci -> hss.counts[:, ci], (a, b) -> a .+= b, hss.index, init = zeros(RT, depth(hss)))
+    data = counts(hss)
+    vals = mapreduce(ci -> data[:, ci], (a, b) -> a .+= b, hss.index, init = zeros(RT, depth(hss)))
     props = copy(hss.properties)
     if haskey(hss.properties, :LiveTime)
         props[:LiveTime] = length(hss) * hss[:LiveTime]
@@ -218,8 +222,9 @@ end
 
 function Base.sum(hss::HyperSpectrum{T,N}, filt::Function) where {T<:Real,N}
     RT = T isa Int ? Int64 : Float64
+    data = counts(hss)
     vals = mapreduce(
-        ci -> hss.counts[:, ci],
+        ci -> data[:, ci],
         (a, b) -> a .+= b,
         filter(ci -> filt(hss, ci), hss.index),
         init = zeros(RT, depth(hss)),
