773b7822942f5bdabb744323587465ead3816508
Nicholas W. M. Ritchie <nicholas.ritchie@nist.gov>
2021-08-29 10:26:31 -0400

    Spectrum addition and subtraction using +, -

--- src/spectrum.jl
+++ src/spectrum.jl
@@ -173,10 +173,24 @@ end
 Base.:*(a::Real, s::Spectrum) = property!(Spectrum(s.energy, a*s.counts, copy(s.properties)), :Name, "$a⋅$(s[:Name])")
 Base.:*(s::Spectrum, a::Real) = property!(Spectrum(s.energy, a*s.counts, copy(s.properties)), :Name, "$a⋅$(s[:Name])")
 Base.:/(s::Spectrum, a::Real) = property!(Spectrum(s.energy, s.counts/a, copy(s.properties)), :Name, "$(s[:Name])/$a")
-Base.:+(s::Spectrum, a::Real) = property!(Spectrum(s.energy, s.counts .+ a, copy(s.properties)), :Name, "offset($(s[:Name]), $a)")
-Base.:+(a::Real, s::Spectrum) = property!(Spectrum(s.energy, s.counts .+ a, copy(s.properties)), :Name, "offset($(s[:Name]), $a)")
-Base.:-(s::Spectrum, a::Real) = property!(Spectrum(s.energy, s.counts .- a, copy(s.properties)), :Name, "offset($(s[:Name]), -$a)")
-Base.:-(a::Real, s::Spectrum) = property!(Spectrum(s.energy, a .- s.counts, copy(s.properties)), :Name, "offset(-$(s[:Name]), $a)")
+
+"""
+    offset(s::Spectrum, dcounts::Real)
+
+Returns a `Spectrum` like `s` but with dcounts added to each channel.
+"""
+offset(s::Spectrum, dcounts::Real) = property!(Spectrum(s.energy, s.counts .+ dcounts, copy(s.properties)), :Name, "$(s[:Name]) offset by $dcounts")
+
+function Base.:+(s1::Spectrum, s2::Spectrum) 
+    @assert isequal(s1.energy,s2.energy) "The energy calibration must be the same to add two spectra."
+    r = Base.OneTo(min(length(s1.counts),length(s2.counts)))
+    property!(Spectrum(s1.energy, view(s1.counts,r) + view(s2.counts,r), commonproperties(s1,s2)), :Name, "$(s1[:Name]) + $(s2[:Name])")
+end
+function Base.:-(s1::Spectrum, s2::Spectrum) 
+    @assert isequal(s1.energy,s2.energy) "The energy calibration must be the same to subtract one spectrum from another."
+    r = Base.OneTo(min(length(s1.counts),length(s2.counts)))
+    property!(Spectrum(s1.energy, view(s1.counts,r) - view(s2.counts,r), commonproperties(s1,s2)), :Name, "$(s1[:Name]) - $(s2[:Name])")
+end
 
 
 """
@@ -869,6 +883,7 @@ Outputs a description of the data in the spectrum.
 """
 function details(io::IO, spec::Spectrum)
     println(io, "           Name:   $(spec[:Name])")
+    println(io, "    Calibration:   $(spec.energy)")
     println(io, "    Beam energy:   $(get(spec, :BeamEnergy, missing)/1000.0) keV")
     println(io, "  Probe current:   $(get(spec, :ProbeCurrent, missing)) nA")
     println(io, "      Live time:   $(get(spec, :LiveTime, missing)) s")
@@ -933,14 +948,14 @@ function commonproperties(props1::Dict{Symbol,Any}, props2::Dict{Symbol,Any})
     end
     return res
 end
-
-function commonproperties(specs::AbstractArray{<:Spectrum})
+function commonproperties(specs::Vararg{<:Spectrum})
     props = specs[1].properties
     for sp2 in specs[2:end]
         props = commonproperties(props, sp2.properties)
     end
     return props
 end
+commonproperties(specs::AbstractArray{<:Spectrum}) = commonproperties(specs...)
 
 """
     maxspectrum(specs::AbstractArray{Spectrum{T}})::Spectrum{T} where T<:Real
@@ -1212,18 +1227,17 @@ end
 Allows changing the energy scale on a spectrum from one LinearEnergyScale to another as though the spectrum were 
 measured on a different detector.  The algorith uses a FFT-base scheme to rescale and shift the spectral data.
 Ths scheme allows for fractional shifts of offset and fractional changes in the width.  It is limited in that
-the change in width must produce an integral number of channels in the resulting spectrum.  The result will 
-have a LinearEnergyScale slightly different from the requested one. ("Life isn't always fair.") The algorithm 
-also attempts to maintain the total spectrum integral so the new spectrum can be used for quantitative purposes.
+the change in width must produce an integral number of channels in the resulting spectrum.  The algorithm 
+maintains the total spectrum integral so the new spectrum can be used for quantitative purposes.
 Plotting one spectrum over the other should maintain peak position but is likely to change the channel counts.
 """
 function recalibrate(s::Spectrum{T}, es::LinearEnergyScale) where { T <: Real }
     (!(s.energy isa LinearEnergyScale)) && error("The rescale(...) function requires that the spectrum has a LinearEnergyScale.")
     # Resample the spectrum onto a new channel width (offset remains same)
-    cxs = counts(s)
+    cxs, oldsum = counts(s), sum(s.counts)
     oldlen, newlen = length(cxs), round(Int, (s.energy.width/es.width)*length(cxs))
     if newlen ≠ length(cxs)
-        cxs = FourierTools.resample(cxs, newlen)*(oldlen/newlen)
+        cxs = FourierTools.resample(cxs, newlen)
     end
     newWidth = s.energy.width*(oldlen/newlen)
     # Shift the first channel from s.energy.offset to es.offset 
@@ -1231,14 +1245,15 @@ function recalibrate(s::Spectrum{T}, es::LinearEnergyScale) where { T <: Real }
     if abs(shft) > 0.01
         cxs = FourierTools.shift(cxs, shft)
     end 
-    return Spectrum(LinearEnergyScale(es.offset, newWidth), cxs, copy(s.properties))
+    return Spectrum(es, cxs*(oldsum/sum(cxs)), copy(s.properties))
 end
 
 """
-    shift(s::Spectrum, ev::Float64)::Spectrum
+    shift(s::Spectrum, ev::AbstractFloat)::Spectrum
 
-Shift the entire spectrum along the energy axis by a specified number of ev.
+Shift the entire spectrum along the energy axis by a specified number of ev by modifying the
+counts data.  This function can shift by a fractional number of channels.
 """
-function shift(s::Spectrum, ev::Float64)::Spectrum
+function shift(s::Spectrum, ev::AbstractFloat)::Spectrum
     return Spectrum(s.energy, FourierTools.shift(counts(s), ev/s.energy.width), copy(s.properties))
 end
