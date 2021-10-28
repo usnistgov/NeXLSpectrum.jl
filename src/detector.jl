bfdb2f3f0f38163c0e47851c67c7d1a3f6a29b68
Nicholas Ritchie <nicholas.ritchie@nist.gov>
2021-10-27 21:17:39 -0400

    Replace number with constants

--- src/detector.jl
+++ src/detector.jl
@@ -152,7 +152,7 @@ resolution(eV::Float64, res::MnKaResolution) =
 
 Converts full-width half-max to Gaussian width.
 """
-gaussianwidth(fwhm::Float64) = fwhm / 2.354820045030949382023138652918
+gaussianwidth(fwhm::Float64) = fwhm / (2.0*sqrt(2.0*log(2.0)))
 
 """"
     profile(energy::Float64, xrayE::Float64, res::Resolution)
